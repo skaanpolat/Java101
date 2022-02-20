c6157f2977d2f71cd2e7c5d604b24b56a4c07cd4
skaanpolat <83609705+skaanpolat@users.noreply.github.com>
2022-02-20 15:28:33 +0300

    kkk

--- Java101/BasitHesapMakinesi.java
+++ Java101/BasitHesapMakinesi.java
@@ -1,47 +1,47 @@
-//package Java101;
-//
-//import java.util.Scanner;
-//
-//public class BasitHesapMakinesi {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        int n1, n2, choice;
-//
-//        System.out.println("**************************\n   Basit Hesap Makinesi\n**************************" +
-//                "\nYapılacak işlemi seçiniz.\n1-) Toplama\n2-) Çıkartma\n3-) Çarpma\n4-) Bölme");
-//        choice = scanner.nextInt();
-//
-//        switch (choice){
-//            case 1:
-//                System.out.println("İşlem yapılacak 1. sayıyı giriniz");
-//                n1 =scanner.nextInt();
-//                System.out.println("İşlem yapılacak 2. sayıyı giriniz");
-//                n2 =scanner.nextInt();
-//                System.out.println("Sonuc:" + (n1+n2));
-//                break;
-//            case 2:
-//                System.out.println("İşlem yapılacak 1. sayıyı giriniz");
-//                n1 =scanner.nextInt();
-//                System.out.println("İşlem yapılacak 2. sayıyı giriniz");
-//                n2 =scanner.nextInt();
-//                System.out.println("Sonuc:" + (n1-n2));
-//                break;
-//            case 3:
-//                System.out.println("İşlem yapılacak 1. sayıyı giriniz");
-//                n1 =scanner.nextInt();
-//                System.out.println("İşlem yapılacak 2. sayıyı giriniz");
-//                n2 =scanner.nextInt();
-//                System.out.println("Sonuc:" + (n1*n2));
-//                break;
-//            case 4:
-//                System.out.println("İşlem yapılacak 1. sayıyı giriniz");
-//                n1 =scanner.nextInt();
-//                System.out.println("İşlem yapılacak 2. sayıyı giriniz");
-//                n2 =scanner.nextInt();
-//                System.out.println("Sonuc:" + (n1/n2));
-//                break;
-//            default:
-//                System.out.println("Lütfen geçerli bir işlem seçiniz!");
-//        }
-//    }
-//}
+package Java101;
+
+import java.util.Scanner;
+
+public class BasitHesapMakinesi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n1, n2, choice;
+
+        System.out.println("**************************\n   Basit Hesap Makinesi\n**************************" +
+                "\nYapılacak işlemi seçiniz.\n1-) Toplama\n2-) Çıkartma\n3-) Çarpma\n4-) Bölme");
+        choice = scanner.nextInt();
+
+        switch (choice){
+            case 1:
+                System.out.println("İşlem yapılacak 1. sayıyı giriniz");
+                n1 =scanner.nextInt();
+                System.out.println("İşlem yapılacak 2. sayıyı giriniz");
+                n2 =scanner.nextInt();
+                System.out.println("Sonuc:" + (n1+n2));
+                break;
+            case 2:
+                System.out.println("İşlem yapılacak 1. sayıyı giriniz");
+                n1 =scanner.nextInt();
+                System.out.println("İşlem yapılacak 2. sayıyı giriniz");
+                n2 =scanner.nextInt();
+                System.out.println("Sonuc:" + (n1-n2));
+                break;
+            case 3:
+                System.out.println("İşlem yapılacak 1. sayıyı giriniz");
+                n1 =scanner.nextInt();
+                System.out.println("İşlem yapılacak 2. sayıyı giriniz");
+                n2 =scanner.nextInt();
+                System.out.println("Sonuc:" + (n1*n2));
+                break;
+            case 4:
+                System.out.println("İşlem yapılacak 1. sayıyı giriniz");
+                n1 =scanner.nextInt();
+                System.out.println("İşlem yapılacak 2. sayıyı giriniz");
+                n2 =scanner.nextInt();
+                System.out.println("Sonuc:" + (n1/n2));
+                break;
+            default:
+                System.out.println("Lütfen geçerli bir işlem seçiniz!");
+        }
+    }
+}
