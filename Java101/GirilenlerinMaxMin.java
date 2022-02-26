a828f2d6c856e0cc50e1e65ec66434e5ae48187a
skaanpolat <83609705+skaanpolat@users.noreply.github.com>
2022-02-27 02:27:32 +0300

    Proje dosyası eklendi.

--- Java101/GirilenlerinMaxMin.java
+++ Java101/GirilenlerinMaxMin.java
@@ -1,4 +1,26 @@
 package Java101;
 
+import java.util.Scanner;
+
 public class GirilenlerinMaxMin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputAmount , number, maxInput = 0, minInput = 0;
+        System.out.println("Kaç adet sayı gireceksiniz? ");
+        inputAmount = scanner.nextInt();
+
+        for (int i =1 ; i<=inputAmount; i++){
+            System.out.println(i + ". sayıyı giriniz : ");
+            number = scanner.nextInt();
+            if (number>maxInput){
+                maxInput=number;
+            }
+            if(number<minInput){
+                minInput=number;
+            }
+        }
+        System.out.print("En büyük sayı : " + maxInput);
+        System.out.println("\nEn küçük sayı : " + minInput);
+
+    }
 }
