39c2046187bb12086fb633a2b1dbcae2e9bc82ae
skaanpolat <83609705+skaanpolat@users.noreply.github.com>
2022-03-01 02:27:53 +0300

    reformatted.

--- Java101/YuzeKadarAsalSayi.java
+++ Java101/YuzeKadarAsalSayi.java
@@ -4,9 +4,7 @@ public class YuzeKadarAsalSayi {
     public static void main(String[] args) {
 
         int sayi, asal, temp;
-        for (
-                int i = 2;
-                i < 101; i++) {
+        for (int i = 2; i < 101; i++) {
             temp = 0;
             for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
