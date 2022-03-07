636484837d70cc75afeb4f86f672fddb2038e9dc
skaanpolat <83609705+skaanpolat@users.noreply.github.com>
2022-03-07 17:54:45 +0300

    Proje dosyası eklendi.

---
+++ Java101/RecursiveDesen.java
@@ -0,0 +1,29 @@
+package Java101;
+
+import java.util.Scanner;
+public class RecursiveDesen {
+
+    static int a=0;
+    static void desen(int num,int nm)
+    {
+        System.out.print(num+" ");
+
+        if(num>0 && a==0)
+        {
+            desen(num-5,nm);
+        }
+        else if (num<=0 || a==1)
+        {
+            a=1;
+            if(num<nm)
+                desen(num+5,nm);
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Sayı giriniz:");
+        int num = scan.nextInt();
+        desen(num,num);
+    }
+}
